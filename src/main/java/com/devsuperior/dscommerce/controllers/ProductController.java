af4c3efcaf3efc0830405e917ce2c164d439e6ab
Guilherme Da Costa Santos <gdcsantos2000@gmail.com>
2023-03-16 14:52:24 -0300

    DELETE product

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -45,4 +45,9 @@ public class ProductController {
         return ResponseEntity.ok(dto);
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
